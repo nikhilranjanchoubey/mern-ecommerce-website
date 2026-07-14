4af4c671e7f173488f7a9e1a7d07a41658c1ca14
Nikhil Ranjan Choubey <ranjannikhil476@gmail.com>
2026-07-14 23:24:23 +0530

    Add navbar icons/dropdown & clear placeholders

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { assets } from "../assets/frontend_assets/assets";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
@@ -25,6 +25,24 @@ const Navbar = () => {
                 <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
             </NavLink>
        </ul>
+       <div className = 'flex items-center gap-6'>
+        <img src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
+        <div className='group relative'>
+            <img src={assets.profile_icon} className='w-5 cursor-pointer' alt="" />
+            <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+                <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
+                    <p className='cursor-pointer hover:text-black'>My Profile</p>
+                    <p className='cursor-pointer hover:text-black'>Orders</p>
+                    <p className='cursor-pointer hover:text-black'>Logout</p>
+                    
+                </div>
+            </div>
+        </div>
+        <Link to='/cart' className='relative'>
+            <img src={assets.cart_icon} className='w-5 min-w-5' alt="" />
+            <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
+        </Link>
+       </div>
     </div>
   )
 }
