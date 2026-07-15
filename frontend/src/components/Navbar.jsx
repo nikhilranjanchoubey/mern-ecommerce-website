3477bee90b2a808fee689485490cb6384b1a4b8f
Nikhil Ranjan Choubey <ranjannikhil476@gmail.com>
2026-07-15 22:18:45 +0530

    Add mobile sidebar menu with toggle

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,10 @@
 import { assets } from "../assets/frontend_assets/assets";
 import { Link, NavLink } from "react-router-dom";
+import { useState } from "react";
 const Navbar = () => {
+
+    const [visible,setVisible] = useState(false);
+
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
 
@@ -42,7 +46,18 @@ const Navbar = () => {
             <img src={assets.cart_icon} className='w-5 min-w-5' alt="" />
             <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
         </Link>
+        <img onClick={()=>setVisible(true)} src={assets.menu_icon} className= 'w-5 cursor-pointer sm:pointer' alt="" />
        </div>
+
+        {/* Sidebar menu for small screens */}
+      <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? "w-full" : "w-0" }`}>
+            <div className='flex flex-col text-gray-600'>
+                <div onClick={()=>setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
+                    <img className='h-4 rotate-180' src={assets.dropdown_icon} alt="" />
+                        <p>Back</p>
+                </div>
+            </div>
+    </div>
     </div>
   )
 }
