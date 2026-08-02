cee47f32bcebcadb2b77d5d92d15837c63d8e619
Nikhil Ranjan Choubey <ranjannikhil476@gmail.com>
2026-08-02 22:43:25 +0530

    Add NewsletterBox to About & Contact; update copy

--- frontend/src/components/NewsletterBox.jsx
+++ frontend/src/components/NewsletterBox.jsx
@@ -6,18 +6,18 @@ const NewsletterBox = () => {
         event.preventDefault();
     }
 
-  return (
-    <div className='text-center'>
-        <p className='text-2xl font-medium text-gray-800'>
-            Subscribe now & get 20% off
-        </p>
-        <p clasName='text-gray-400 mt-3'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit, quos.\</p>
-        <form onSubmit={onSubmitHandler} className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3'>
-            <input className='w-full sm:flex-1 outline-none' type="email" placeholder='Enter your email' required/>
-            <button type='submit' className='bg-black text-white text-xs px-10 py-4' >SUBSCRIBE</button>
-        </form>
-    </div>
-  )
+    return (
+        <div className='text-center'>
+            <p className='text-2xl font-medium text-gray-800'>
+                Subscribe now & get 20% off
+            </p>
+            <p clasName='text-gray-400 mt-3'>Be the first to discover our latest collections, exclusive deals, and new arrivals delivered straight to your inbox. Stay ahead of the trends with Forever.</p>
+            <form onSubmit={onSubmitHandler} className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3'>
+                <input className='w-full sm:flex-1 outline-none' type="email" placeholder='Enter your email' required />
+                <button type='submit' className='bg-black text-white text-xs px-10 py-4' >SUBSCRIBE</button>
+            </form>
+        </div>
+    )
 }
 
 export default NewsletterBox
