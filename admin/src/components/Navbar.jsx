d2f2634ac7ded804b2ad682f2e1bc116035180e2
Nikhil Ranjan Choubey <ranjannikhil476@gmail.com>
2026-08-12 22:23:18 +0530

    Add admin panel with authentication

---
+++ admin/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import { assets } from '../assets/assets'
+
+const Navbar = () => {
+    return (
+        <div>
+            <img src={assets.logo} alt="" />
+            <button>Logout</button>
+        </div>
+    )
+}
+
+export default Navbar
